7f24818b765c7426d28b5ddf370c205686a27e0c
DaLexusS <jenya.flieshman@gmail.com>
2024-07-30 18:08:12 +0300

    added assets

--- Assets/Scripts/Player/PlayerProfile.cs
+++ Assets/Scripts/Player/PlayerProfile.cs
@@ -5,12 +5,15 @@ public class PlayerProfile
 {
     public string name;
     public int age;
+    public int BestScore;
     public string photoPath;
+    
 
-    public PlayerProfile(string name, int age, string photoPath)
+    public PlayerProfile(string name, int age, int bestScore, string photoPath)
     {
         this.name = name;
         this.age = age;
         this.photoPath = photoPath;
+        this.BestScore = bestScore;
     }
 }
